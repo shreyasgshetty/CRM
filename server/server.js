7a746a14e44ad5de293c63af59158f29fe6451bd
Shreyas G Shetty <shreyasgshetty.18@gmail.com>
2025-10-28 23:38:10 +0530

    initialization

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Sample route
+app.get("/", (req, res) => res.send("CRM API is running..."));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));

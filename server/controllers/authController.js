03eb7cd1755a3faecd0683542278537b807d1f9e
Shreyas G Shetty <shreyasgshetty.18@gmail.com>
2025-10-30 18:38:01 +0530

    login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -12,24 +12,6 @@ export const registerUser = async (req, res) => {
   }
 };
 
-export const loginUser = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (!user) return res.status(404).json({ message: "User not found" });
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(401).json({ message: "Invalid password" });
-
-    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
-      expiresIn: "1d",
-    });
-
-    res.json({ token, role: user.role, name: user.name });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
 export const loginBusinessManager = async (req, res) => {
   try {
     const { email, password } = req.body;
